31cce2a715d038f32caf332e6259257d4521fc2b
habib <habibdev96@gmail.com>
2021-06-26 18:43:11 -0600

    created about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from './components/Header/Header';
+import About from './components/About/About';
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <About />
+      </main>
     </>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,23 @@
+import './About.scss';
+import img from '../../images/desktop/image-interactive.jpg';
+
+const About = () => {
+  return (
+    <section className='about'>
+      <div className='about-content'>
+        <img src={img} alt='man with vr headset' className='about-img' />
+        <div className='about-info'>
+          <h2 className='about-heading'>The leader in interactive VR</h2>
+          <p className='about-desc'>
+            Founded in 2011, Loopstudios has been producing world-class virtual
+            reality projects for some of the best companies around the globe.
+            Our award-winning creations have transformed businesses through
+            digital experiences that bind to their brand.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;

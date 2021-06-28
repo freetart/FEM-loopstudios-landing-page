f4bc92fe144355af2b852defb675c2316e8dffd1
habib <habibdev96@gmail.com>
2021-06-27 22:10:50 -0600

    created preloader in index.html

--- src/App.js
+++ src/App.js
@@ -6,22 +6,11 @@ import About from './components/About/About';
 import Gallery from './components/Gallery/Gallery';
 import Footer from './components/Footer/Footer';
 import Tag from './components/Tag/Tag';
-import Preloader from './components/Preloader/Preloader';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
   const [isActive, setIsActive] = useState(false);
-
   AOS.init({ offset: 100, duration: 1000, once: true });
-
-  useEffect(() => {
-    setIsLoading(false);
-  }, []);
-
-  if (isLoading) {
-    return <Preloader />;
-  }
 
   return (
     <>
